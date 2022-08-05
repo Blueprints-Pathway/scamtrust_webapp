bc8b882cbb756a17d55cdd79ac08eb729a13c970
laribright <laribright8@gmail.com>
2022-08-05 15:45:16 +0100

    Make header content dynamic

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,12 +3,12 @@ import Header from "./Header";
 import Sidenav from "./Sidenav";
 
 const Layout = (props) => {
-  const { children } = props;
+  const { children, heading } = props;
 
   return (
     <>
       <nav className="fixed pl-[212.25px] 2xl:pl-[283px] z-20 w-full mb-[50px] bg-white h-[80px] 2xl:h-[133px] top-0 left-0">
-        <Header />
+        <Header heading={heading} />
       </nav>
       <aside className="w-[212.25px] z-30 2xl:w-[283px] top-0 left-0 bg-colorPrimary fixed h-screen">
         <Sidenav />
