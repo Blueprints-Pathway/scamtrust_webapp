4d16b935c645a016bd593ed7edd40d209f40218e
laribright <laribright8@gmail.com>
2022-08-12 07:34:39 +0100

    Complete vendor details styling

--- src/pages/vendor/VendorDetails.jsx
+++ src/pages/vendor/VendorDetails.jsx
@@ -1,6 +1,11 @@
 import Layout from "../../components/Layout/Layout";
 import Shield from "../../assets/images/svg/shield.svg";
 import Add from "../../assets/images/svg/add.svg";
+import Chat from "../../assets/images/svg/chat.svg";
+import Message from "../../assets/images/svg/message.svg";
+import Check from "../../assets/images/svg/check.svg";
+import Location from "../../assets/images/svg/location.svg";
+import Phone from "../../assets/images/svg/phone.svg";
 
 const VendorDetails = () => {
   return (
@@ -14,7 +19,7 @@ const VendorDetails = () => {
           />
           <div className="absolute flex items-center left-[60px] -bottom-[40%] w-full">
             <div className="bg-white w-[139px] relative overflow-hidden rounded-full h-[139px] shadow-md p-1">
-              <div className="bg-[#D3530B] grid place-content-center text-white absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rounded-full w-[132px] h-[132px]">
+              <div className="bg-[#D3530B] font-semibold text-[40px] grid place-content-center text-white absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rounded-full w-[132px] h-[132px]">
                 RC
               </div>
             </div>
@@ -36,7 +41,38 @@ const VendorDetails = () => {
           </div>
         </div>
 
-        <div className="pt-36 px-28 bg-white">
+        <div className="pt-36 px-28 bg-white mb-9 pb-9 rounded-b-[25px]">
+          <div className="">
+            <div className="px-5 mb-9 flex justify-between items-center border-b-2 border-b-[#EAEAEA]">
+              <div className="flex">
+                <img src={Message} alt="message_icon" className="mr-6" />
+                <span className="font-medium text-2xl">Email address</span>
+              </div>
+              <p>Ridic@gmail.com</p>
+            </div>
+            <div className="px-5 mb-9 flex justify-between items-center border-b-2 border-b-[#EAEAEA]">
+              <div className="flex">
+                <img src={Phone} alt="message_icon" className="mr-6" />
+                <span className="font-medium text-2xl">Phone number</span>
+              </div>
+              <p>08156711115</p>
+            </div>
+            <div className="px-5 mb-9 flex justify-between items-center border-b-2 border-b-[#EAEAEA]">
+              <div className="flex">
+                <img src={Location} alt="message_icon" className="mr-6" />
+                <span className="font-medium text-2xl">Office address</span>
+              </div>
+              <p>Ikate, Lekki, Lagosm</p>
+            </div>
+            <div className="px-5 mb-9 flex justify-between items-center border-b-2 border-b-[#EAEAEA]">
+              <div className="flex">
+                <img src={Check} alt="message_icon" className="mr-6" />
+                <span className="font-medium text-2xl">RC Number</span>
+              </div>
+              <p>7654321</p>
+            </div>
+          </div>
+
           <div className="flex justify-between">
             <div className="bg-[rgba(237,237,237,0.4)] py-4 px-11 w-[624px] rounded-2xl">
               {RATING.map((serv) => {
@@ -49,7 +85,31 @@ const VendorDetails = () => {
                 );
               })}
             </div>
-            <div>RIGHT</div>
+            <div>
+              <div className="bg-[#F8F8F8] w-[296px] rounded-2xl">
+                <div className="flex items-center justify-between px-2 py-4">
+                  <p className="font-medium">Competence Score</p>
+                  <p className="bg-colorGreen text-white py-2 w-11 text-center rounded-lg">
+                    80%
+                  </p>
+                </div>
+                <div className="flex items-center justify-between px-2 py-4">
+                  <p className="font-medium">Grade</p>
+                  <p className="bg-colorGreen text-white w-11 text-center py-2 rounded-lg">
+                    A
+                  </p>
+                </div>
+              </div>
+              <div className="mt-3 p-6 flex items-center bg-[#E2D8F1] text-colorPrimary w-[296px] rounded-2xl">
+                <div className="w-10 h-10 bg-colorPrimary grid place-content-center mr-3 rounded-full">
+                  <img src={Chat} alt="report_vendor" />
+                </div>
+                <div>
+                  <p className="font-semibold text-lg">Report vendor</p>
+                  <p>Chat with us</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
