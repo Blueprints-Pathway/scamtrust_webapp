fac147e3ed6048fbed2083cd9030c0256cc59f4c
laribright <laribright8@gmail.com>
2022-08-19 08:05:19 +0100

    Fund wallet, position fixed

--- src/components/Pages/Wallet/FundWallet.jsx
+++ src/components/Pages/Wallet/FundWallet.jsx
@@ -1,14 +1,28 @@
+import { useEffect } from "react";
+
 import Copy from "../../../assets/images/svg/copy.svg";
 
 const FundWallet = (props) => {
   const { showFundWallet, fundWalletHandler } = props;
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (window) document.body.style.overflow = "hidden";
+    }
+
+    if (!showFundWallet) {
+      if (typeof window !== "undefined") {
+        if (window) document.body.style.overflow = "unset";
+      }
+    }
+  }, [showFundWallet]);
 
   if (!showFundWallet) return <></>;
 
   return (
     <div
       onClick={fundWalletHandler}
-      className="w-screen h-screen bg-[rgba(23,23,23,0.54)] z-50 grid place-content-center absolute left-0 top-0"
+      className="w-screen fixed h-screen bg-[rgba(23,23,23,0.54)] z-50 grid place-content-center left-0 top-0"
     >
       <div className="rounded-2xl text-center text-colorPrimary bg-white max-w-xs md:max-w-lg px-11 py-3">
         <p className="mt-6 mb-3 text-xl">Fund your ScamTrust Wallet</p>
