eb587f15ba7bd3ff39c56f98d12dcf17167325e7
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-01-15 09:40:54 +0100

    sunday commit

--- src/components/Pages/CustDashboard/AnimeList.jsx
+++ src/components/Pages/CustDashboard/AnimeList.jsx
@@ -3,10 +3,10 @@
 function AnimeList() {
 	return (
 		<div>
-			<div className="w-[200px] mx-auto hover:scale-105 transition-all duration-500 2xl:w-[280px] py-[31px] px-[35px] bg-[#ffffff]  rounded-2xl">
+			<div className="w-[200px] mx-auto md:px-1 hover:scale-105 transition-all duration-500 2xl:w-[280px] py-[31px] px-[35px] bg-[#ffffff]  rounded-2xl">
 				<div className="border my-5 w-[200px] mx-auto hover:scale-105 transition-all duration-500 2xl:w-[280px] py-[31px] px-[35px] bg-[#ffffff]  rounded-2xl">
         <i class="    p-3 fa fa-phone" aria-hidden="true"></i>
-					<span className="py-1">call us </span>
+					<span className="py-1">Call us now</span>
 					<br />
 					<span className="p-3">+2348169199932</span>
 				</div>
@@ -18,18 +18,17 @@ function AnimeList() {
 						href="https://wa.me/+23407067302238"
 					>
 						<i class="fa fa-whatsapp whatsapp-icon"></i>
-
 						<span className="p-3">Whatsapp</span>
 						<br />
-						<span className="p-3">Chat with Dami</span>
+						<span className="p-3">Chat with Dami </span>
 					</a>
 				</div>
 				<div className="border w-[200px] mx-auto hover:scale-105 transition-all duration-500 2xl:w-[280px] py-[31px] px-[35px] bg-[#ffffff]  rounded-2xl my-5">
 					<a  className="p-3" href="mailto:Thescamtrust@amall.com">
 						<i class="fa fa-envelope" aria-hidden="true"></i>
-						<span  className="p-3">email</span>
+						<span  className="p-3">Email</span>
 						<br />
-						<span  className="p-3">Thescamtrust@amall.com</span>
+						<span  className="p-3">Thescamtrust@gmail.com</span>
 					</a>
 				</div>
 			</div>
