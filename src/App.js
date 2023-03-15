9073165542889c3673005ff847a145ebbe71936a
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-03-15 14:34:41 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
+import About from './pages/landing/scamtrust-landing-page/src/components/About/About'
+import LandingPage from './pages/landing/scamtrust-landing-page/src/Home'
 import AppLayout from './components/layout/AppLayout'
 
 const App = () => {
   return (
 	<Routes>
-		<Route path='/' element={<AppLayout />} />
+		<Route path='/' element={<LandingPage />} />
+		<Route path='/about' element={<About />} />
+		<Route path='/scamtrust' element={<AppLayout /> } />
 	</Routes>
   )
 }
 
-export default App
+export default App          
