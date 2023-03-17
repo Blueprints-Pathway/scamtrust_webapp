fd48668f8b8a698541690b038699a83205390e6e
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-03-17 09:36:30 +0100

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import About from './pages/landing/scamtrust-landing-page/src/components/About/About'
-import LandingPage from './pages/landing/scamtrust-landing-page/src/Home'
-import CustomerDashboard from './pages/dashboard/CustomerDashboard/CustomerDashboard'
 import AppLayout from './components/layout/AppLayout'
+import HomePage from './pages/landing/scamtrust-landing-page/src/Home'
 
 const App = () => {
   return (
 	<Routes>
-		<Route path='/' element={<LandingPage />} />
+		<Route path='/' element={<HomePage />} />
 		<Route path='/about' element={<About />} />
 		<Route path='/layout' element={ <AppLayout /> } />
 	</Routes>
