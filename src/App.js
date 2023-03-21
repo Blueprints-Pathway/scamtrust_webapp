5fa4de3113fda18f4fd714351d5d568cefa5a8a0
mackbook pro <mackbookpro@mackbooks-MacBook-Pro.local>
2023-03-21 12:51:59 +0100

    collins commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,9 @@ import AppLayout from "./components/layout/AppLayout"
 import About from "./pages/home/src/components/About/About"
 import Faq from "./pages/home/src/components/Faq/Faq"
 import KnowMore from "./pages/home/src/components/knowMore/KnowMore"
-import HomePage from "./pages/home/src/HomePage"
 import VendorDashboard from "./pages/dashboards/VendorDashboard/VendorDashboard"
 import Sidebar from "./components/sidebar/Sidebar"
 import Vendordashboard from "./components/vendordashboard/Vendordashboard"
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-import AppLayout from './components/Layout/AppLayout'
-import About from './pages/home/src/components/About/About'
-import Faq from './pages/home/src/components/Faq/Faq'
-import KnowMore from './pages/home/src/components/knowMore/KnowMore'
 import HomePage from './pages/home/src/HomePage'
 import Signin from './pages/sign-in/Signin'
 import Vendorsignup from './pages/sign-up/vendor-sign-up/Vendorsignup'
