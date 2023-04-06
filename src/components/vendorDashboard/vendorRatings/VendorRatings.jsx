863931e5fd5b4ee35088fa6f93613fd18007e84e
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-04-06 01:24:58 +0100

    last commit

--- src/components/vendorDashboard/vendorRatings/VendorRatings.jsx
+++ src/components/vendorDashboard/vendorRatings/VendorRatings.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import classes from './VendorRatings.module.css'
 import { Card } from 'antd'
+import { CgShare } from 'react-icons/cg'
+import ratings from '../../../assets/images/ratings.png'
 
 const VendorRatings = () => {
   return (
     <div className={classes['wrapper']}>
-        <Card>
-            
+        <Card className={classes['con']}>
+            <div className={classes['top']}>
+                <p className={classes['top-left']}>Ratings</p>
+                <button className={classes['top-right']}>Share Stats<CgShare className={classes['top-right-icon']} /></button>
+            </div>
+            <div className={classes['bottom']}>
+                <img className={classes['bottom-img']} src={ratings} alt="..." />
+            </div>
         </Card>
     </div>
   )
