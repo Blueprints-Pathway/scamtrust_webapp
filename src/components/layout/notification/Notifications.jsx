08e0c1692b83f9918fe1340bc78d0dd4600c16a0
blueprintsPathway <107635376+blueprintsPathway@users.noreply.github.com>
2023-04-13 02:09:49 +0100

    added layout

---
+++ src/components/layout/notification/Notifications.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import classes from './Notification.module.css'
+import { Dropdown, Badge, Menu } from 'antd';
+
+import { GoPrimitiveDot } from 'react-icons/go';
+import { CiMail } from 'react-icons/ci';
+
+import { MdNotificationsNone } from 'react-icons/md';
+import { useSelector } from 'react-redux';
+
+const Notifications = () => {
+  const notification = useSelector(state => state.notification);
+
+ let menuItem =  notification.unreadNotifications.map((notificationItem, index) => {
+
+      
+    return <div> 
+      <div className={classes['drop1-con']}>
+    <GoPrimitiveDot className={classes['drop-dot']} />
+    <div className={classes['drop-message']}>
+        <p className={classes['message-top']}>{notificationItem.content}</p>
+        <p className={classes['message-bottom']}>{notificationItem.notification_time}</p>
+    </div>
+    <CiMail className={classes['drop-icon2']} />
+  </div>
+  <hr className={classes['hr']} />
+  </div>
+  })
+  
+
+      const menuStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        height: '20rem',
+        overflowY: 'scroll',
+      };
+    
+      const menu = (
+        <Menu style={menuStyle}>
+       
+          {menuItem}
+        </Menu>
+      );
+  return (
+    <div className={classes['wrapper']}>
+    <Dropdown 
+    overlay={menu}
+    placement="topRight"
+    trigger={['click']}
+    >
+      <div className={classes['notificatiion-con']}>
+        <Badge count={notification.unreadNotifications.length}>
+             <div>
+                <MdNotificationsNone className={classes['header-icon']} style={{ fill: '#232164' }} />
+             </div>
+        </Badge>
+      </div>
+    </Dropdown>
+    </div>
+  )
+}
+
+export default Notifications
