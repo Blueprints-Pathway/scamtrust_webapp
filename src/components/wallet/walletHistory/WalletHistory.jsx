49519f164e96c2abe2fa2d369d744975721cf9ae
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-04-15 13:28:46 +0100

    saturday commit

--- src/components/wallet/walletHistory/WalletHistory.jsx
+++ src/components/wallet/walletHistory/WalletHistory.jsx
@@ -4,11 +4,12 @@ import empty from '../../../assets/images/empty.png'
 import Credit from '../credit/Credit'
 import Debit from '../debit/Debit'
 import { useSelector } from 'react-redux'
+import Empty from '../../customerdashboard/CustDashTransaction/empty/Empty'
 
 const WalletHistory = () => {
     const walletTransactions = useSelector(state => state.wallet)
     console.log(walletTransactions.data);
-    let content = walletTransactions?.data?.length === 0 ? <p>YOU HAVE NO TRANSACTIONS!</p> : walletTransactions?.data?.map((transaction) => {
+    let content = walletTransactions?.data?.length === 0 ? <div><Empty /></div> : walletTransactions?.data?.map((transaction) => {
         if(transaction.type === "CREDIT"){
             return <Credit date = {transaction.date}  refNo = {transaction.tracking_reference} purpose = {transaction.purpose} amount = {transaction.amount}  />
         }else{
