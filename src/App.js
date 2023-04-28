75dfa90fe71f555b7a2654efd555da6dcbb33c08
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-04-28 14:27:34 +0100

    updated branch

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ import PasswordRecoveryEmail from "./pages/passwordrecovery/PasswordRecoveryEmai
 import PasswordRecoveryPhone from "./pages/passwordrecovery/PasswordRecoveryPhone";
 import UpdatedPassword from "./pages/passwordrecovery/UpdatedPassword";
 import SettingsVendor from "../src/pages/SettingsVendor/SettingsVendor";
-import VendorDashboard from "./pages/dashboards/VendorDashboard/VendorDashboard";
+import VendorDashboard from "./pages/dashboards/vendorDashboard/VendorDashboard";
 import Support from "./pages/support/Support";
 import Ratting from "./components/transactions/ratting/Ratting";
 import Fundbutton from "./components/buttons/fundbutton/Fundbutton"
